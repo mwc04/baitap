6e29bd4cb6c76d1cf9f1490ada886420874cba32
mwc04 <luongdung1018@gmail.com>
2024-01-24 21:35:04 +0700

    them buy

--- src/App.js
+++ src/App.js
@@ -1,11 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect} from 'react';
+import axios from 'axios';
+import {BrowserRouter, Routes,Route} from 'react-router-dom';
+import BuyPage from './page/buy';
 
 function App() {
+
+  const [sumit, setSumit] = useState({
+    username: null,
+    password: null
+  })
+  const [users,setUsers] = useState([])
+  function onSumit() {
+    console.log(sumit)
+
+    axios.post(`http://localhost:3000/users`, sumit)
+
+  }
+  useEffect(() => {
+    axios.get(`http://localhost:3000/users`).then(function(a){
+      console.log(a)
+      setUsers([...a.data])
+    })
+  }, []);
+
+
+  function handerInput(e) {
+    console.log(e.target.value, e.target.name)
+    let data = ({ ...sumit })
+    data[e.target.name] = e.target.value
+    setSumit({ ...data })
+  }
+
+
   return (
-    <div>
-      <h1>hhalas</h1>
-    </div>
+    <>
+      <div>
+        <label for="username"></label>
+        <input name='username' onChange={handerInput} type='text' placeholder="username" value={sumit.username}></input>
+        <label htmlFor="password"></label>
+        <input name='password' onChange={handerInput} type="password" placeholder="password" value={sumit.password}></input>
+        <button onClick={onSumit}>Create</button>
+
+      </div>
+      <div>
+        <table>
+      <thead>
+        <tr>
+          <th>User</th>
+          <th>Password</th>
+        </tr>
+      </thead>
+      <tbody>
+        {users.map((user, index) => (
+          <tr key={index}>
+            <td>{user.username}</td>
+            <td>{user.password}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+      </div>
+
+      <BrowserRouter>
+        <Routes>
+          <Route path='buy' element = {< BuyPage/>}/> 
+        </Routes>
+      </BrowserRouter>
+    </>
+
   )
 }
 
---
+++ src/page/buy.js
@@ -0,0 +1,46 @@
+
+
+import { useState ,useEffect} from "react";
+import axios from 'axios';
+
+function Buy () {
+    const [sumit, setSumit] = useState({
+        username: null,
+        price: null
+    })
+
+const [users, setUsers] = useState([])
+function onSumit() {
+    console.log(sumit)
+
+    axios.post(`http://localhost:3000/buys`, sumit)
+};
+
+useEffect(() => {
+    axios.get(`http://localhost:3000/buys`).then(function(a){
+      console.log(a)
+      setUsers([...a.data])
+    })
+  }, []);
+
+function handerInput (x) {
+ console.log(x.target.value, x.target.name)
+ let data = ({...sumit})
+ data[x.target.name] = x.target.value
+ setSumit({...data})
+}
+
+    return(
+        <>
+        <div>
+            <input name='username' onChange={handerInput} type='text' placeholder='username' value={sumit.username}></input>
+            <input name="price" onChange={handerInput} type="text" placeholder="bargain price ($)" value={sumit.price}/>
+            <img src = "./imgs/th.jpeg" alt="error" />
+            <button onClick={onSumit}>Create</button>
+        </div>
+        <p>Tong so tien : </p>
+        </>
+    )
+}
+
+export default Buy;
